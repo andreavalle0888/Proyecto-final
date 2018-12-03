83f4a17ed3839953ad61f240f8d6891528ae5dc7
JohnathanRA <31198069+JohnathanRA@users.noreply.github.com>
2018-12-03 15:09:21 -0600

    images agree

--- producto/producto/paginadeinicioViewController.swift
+++ producto/producto/paginadeinicioViewController.swift
@@ -12,7 +12,7 @@ class paginadeinicioViewController: UIViewController, UIScrollViewDelegate{
     
     @IBOutlet weak var pagecontrol: UIPageControl!
     @IBOutlet weak var scrollcontrol: UIScrollView!
-    var galeriaimagenes: [String] = ["1","2","3"]
+    var galeriaimagenes: [String] = ["Template-One","Template-Two","Template-Three"]
     var frame = CGRect(x:0,y:0,width:0,height:0)
     
     override func viewDidLoad() {
@@ -30,13 +30,28 @@ class paginadeinicioViewController: UIViewController, UIScrollViewDelegate{
         scrollcontrol.contentSize = CGSize(width: scrollcontrol.frame.size.width * CGFloat(galeriaimagenes.count), height: scrollcontrol.frame.size.height)
         scrollcontrol.delegate = self
     }
-
-    
-        func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-            var pagenumber = scrollcontrol.contentOffset.x / scrollcontrol.frame.size.width
-            pagecontrol.currentPage = Int(pagenumber)
-            
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            stoppedScrolling()
         }
-
+    }
+    
+    @objc func hadleSwipe(gesture: UISwipeGestureRecognizer){
+        let originalLocation = self.scrollcontrol.center
+        if gesture.direction == UISwipeGestureRecognizer.Direction.left{
+            performSegue(withIdentifier: "toAplication", sender: nil)
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        var pagenumber = scrollcontrol.contentOffset.x / scrollcontrol.frame.size.width
+        pagecontrol.currentPage = Int(pagenumber)
+        stoppedScrolling()
+    }
+    
+    func stoppedScrolling() {
+        let swipeRigthGesture = UISwipeGestureRecognizer(target: self, action: #selector(hadleSwipe(gesture:)))
+        swipeRigthGesture.direction = UISwipeGestureRecognizer.Direction.left
+        scrollcontrol.addGestureRecognizer(swipeRigthGesture)
+    }
 }
-
