15fc94a6dae006cfb5fadc6ba6e6f55ea8f606d6
Alejandra-Toriz <42162756+Alejandra-Toriz@users.noreply.github.com>
2018-12-05 16:04:34 -0600

    Add files via upload

--- producto/producto/paginadeinicioViewController.swift
+++ producto/producto/paginadeinicioViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 
 class paginadeinicioViewController: UIViewController, UIScrollViewDelegate{
     
+
     @IBOutlet weak var pagecontrol: UIPageControl!
     @IBOutlet weak var scrollcontrol: UIScrollView!
     var galeriaimagenes: [String] = ["Template-One","Template-Two","Template-Three"]
     var frame = CGRect(x:0,y:0,width:0,height:0)
+    var one: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let oneTime = UserDefaults.standard
+        
+        
+        if oneTime.integer(forKey: "oneTime") == 0{
         pagecontrol.numberOfPages = galeriaimagenes.count
         for index in 0 ..< galeriaimagenes.count{
             frame.origin.x = scrollcontrol.frame.size.width * CGFloat(index)
@@ -25,41 +30,49 @@ class paginadeinicioViewController: UIViewController, UIScrollViewDelegate{
             let imgView = UIImageView(frame: frame)
             imgView.image = UIImage(named: galeriaimagenes[index])
             self.scrollcontrol.addSubview(imgView)
-        }
-        print("a")
         
         scrollcontrol.contentSize = CGSize(width: scrollcontrol.frame.size.width * CGFloat(galeriaimagenes.count), height: scrollcontrol.frame.size.height)
         scrollcontrol.delegate = self
-        print("b")
+        }
+        
+        one = oneTime.integer(forKey: "oneTime") + 1
+        oneTime.set(one, forKey: "oneTime")
+        }
+        else{
+            perform(#selector(goTo), with: nil, afterDelay: 0)
+        }
     }
+    
+    @objc func goTo() {
+        performSegue(withIdentifier: "toAplication", sender: nil)
+    }
+    
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         if !decelerate {
             stoppedScrolling()
         }
-        print("c")
     }
     
     @objc func hadleSwipe(gesture: UISwipeGestureRecognizer){
         let originalLocation = self.scrollcontrol.center
         if gesture.direction == UISwipeGestureRecognizer.Direction.left{
             performSegue(withIdentifier: "toAplication", sender: nil)
-            print("m")
         }
-        print("d")
+
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         var pagenumber = scrollcontrol.contentOffset.x / scrollcontrol.frame.size.width
         pagecontrol.currentPage = Int(pagenumber)
         stoppedScrolling()
-        print("e")
+
     }
     
     func stoppedScrolling() {
+        
         let swipeRigthGesture = UISwipeGestureRecognizer(target: self, action: #selector(hadleSwipe(gesture:)))
         swipeRigthGesture.direction = UISwipeGestureRecognizer.Direction.left
         scrollcontrol.addGestureRecognizer(swipeRigthGesture)
-        print("f")
+        
     }
-    
 }
